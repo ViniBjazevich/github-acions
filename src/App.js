b3b5a32f39b261394c768b9b5a15d16c4f108779
Vini Bjazevich <vinibjazevich@MacBook-Pro.local>
2023-02-08 12:17:59 -0600

    feat: added linting and coverage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+  return <div className="App">Hello world</div>;
 }
 
 export default App;
